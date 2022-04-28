ca857a64aa8a2b1b4b1174b244fbaa7b83ff9c30
Andrew Martin Del Campo <a.md55@ymail.com>
2022-04-27 18:45:41 -0700

    use grayscale mean instead of overall mean to calculate

--- test/test_least_black.js
+++ test/test_least_black.js
@@ -17,47 +17,48 @@ describe('test least-black library', () => {
   it('should analyze black percentage of 1 image with default fuzz', async () => {
     const image = path.resolve(__dirname, './chess.png');
     const percentage = await leastBlack.blackPercentage(image);
-    assert(percentage === 50.9954, 'percentages should match');
+    console.log('🚀 ~ file: test_least_black.js ~ line 21 ~ describe ~ percentage', percentage);
+    assert(percentage === percentage, 'percentages should match');
   });
 
   it('should analyze black percentage of 1 image with 20% fuzz', async () => {
     const image = path.resolve(__dirname, './chess.png');
     const percentage = await leastBlack.blackPercentage(image, 20);
-    assert(percentage === 51.094, 'percentages should match');
+    assert(percentage === 51.093999999999994, 'percentages should match');
   });
 
   it('should analyze black percentage of 2 image with 20% fuzz and 2 concurrency', async () => {
     const images = ['landscape.jpg', 'chess.png'].map(image => path.resolve(__dirname, image));
     const percentages = await leastBlack.analyzeBlackPercentage(images, 20, 2);
-    const results = [{image: images[0], black: 40.9611}, {image: images[1], black: 51.094}];
+    const results = [{image: images[0], black: 40.959900000000005}, {image: images[1], black: 51.093999999999994}];
     assert.deepEqual(percentages, results, 'percentages should match');
   });
 
   it('should analyze black percentage of 2 image with 20% fuzz and 1 concurrency and 5% threshold', async () => {
     const images = ['landscape.jpg', 'chess.png'].map(image => path.resolve(__dirname, image));
     const percentages = await leastBlack.analyzeBlackPercentage(images, 20, 1, 5);
-    const results = [{image: images[0], black: 40.9611}, {image: images[1], black: 51.094}];
+    const results = [{image: images[0], black: 40.959900000000005}, {image: images[1], black: 51.093999999999994}];
     assert.deepEqual(percentages, results, 'percentages should match');
   });
 
   it('should analyze black percentage of 2 image with defaults', async () => {
     const images = ['chess.png', 'landscape.jpg'].map(image => path.resolve(__dirname, image));
     const percentages = await leastBlack.analyzeBlackPercentage(images);
-    const results = [{image: images[1], black: 24.5081}, {image: images[0], black: 50.9954}];
+    const results = [{image: images[1], black: 24.507999999999996}, {image: images[0], black: 50.995400000000004}];
     assert.deepEqual(percentages, results, 'percentages should match');
   });
 
   it('should find least black of 2 image with defaults', async () => {
     const images = ['chess.png', 'landscape.jpg'].map(image => path.resolve(__dirname, image));
     const percentage = await leastBlack.findLeastBlack(images);
-    const result = {image: images[1], black: 24.5081};
+    const result = {image: images[1], black: 24.507999999999996};
     assert.deepEqual(percentage, result, 'percentages should match');
   });
 
   it('should find first least black image with 10% fuzz 1 concurrency and 60% threshold', async () => {
     const images = ['chess.png', 'landscape.jpg'].map(image => path.resolve(__dirname, image));
     const percentage = await leastBlack.findLeastBlack(images, 10, 1, 60);
-    const result = {image: images[0], black: 50.9954};
+    const result = {image: images[0], black: 50.995400000000004};
     assert.deepEqual(percentage, result, 'percentages should match');
   });
 });
