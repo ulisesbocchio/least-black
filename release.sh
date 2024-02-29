c04aa30116785125a181c1ffd27455e55c89fb27
Uli Bocchio <Sergio.U.Bocchio@Disney.com>
2024-02-29 11:48:19 -0800

    release script

---
+++ release.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $1 != *"major"* ]] && [[ $1 != *"minor"* ]] && [[ $1 != *"patch"* ]]; then
+    echo "usage: ./release major|minor|patch"
+    exit 1
+fi
+
+if [ -n "$(git status --porcelain)" ]; then
+	echo "Git Repository is not clean. Commit/revert all changes prior to release"
+	exit 1
+fi
+
+remote=`git remote`
+branch=`git rev-parse --abbrev-ref HEAD`
+version=`node -e 'console.log(require("./package.json").version)'`
+
+echo "Cleaning Directory..."
+rm -f *.tgz
+
+echo "Setting New Release Version..."
+npm --no-git-tag-version version $1
+new_version=`node -e 'console.log(require("./package.json").version)'`
+git add ./package.json
+git commit -m "New Release Version ${new_version}"
+git push ${remote} ${branch}
+
+echo "Cleaning..."
+yarn run clean
+
+echo "Building..."
+yarn run build
+
+echo "Uploading Artifact..."
+yarn publish
+
+echo "Tagging..."
+git tag -a ${new_version} -m "NPM Package Release ${new_version}"
+git push ${remote} --tags
+
+
+echo "Done!"
